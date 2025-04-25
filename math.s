3a13feca4fed1762db680af7bee75dfe33a30d4f
Will Oliver <willoliver.main@gmail.com>
2025-04-25 17:53:34 +0000

    implement bitwise int sqrt routine & is_fibonacci test

--- math.s
+++ math.s
@@ -8,7 +8,7 @@ global isqrt
 
 factorial:
 	xor rax, rax
-	mov al, dil
+	mov al, dil ; factorial(256) > 2^64
 	xor rdi, rdi
 	mov dil, al
 .loop:
@@ -22,7 +22,7 @@ factorial:
 
 
 fibonacci:
-	and rdi, 0xff
+	and rdi, 0xff ; fibonacci(256) > 2^64
 	xor rax, rax
 	mov rcx, 1
 	mov rdx, 1
@@ -36,6 +36,71 @@ fibonacci:
 	add rdx, rcx
 	sub rdi, 1
 	jmp .loop
+.done:
+	ret
+
+
+; is_fib checks if 5n^2 + 4 or 5n^2 - 4 is perfect square
+is_fib:
+	push r12
+
+	and rdi, 0xffffffff
+	mov rax, rdi
+	mul rdi
+	mov rcx, 5
+	mul rcx
+	mov r12, rax
+	add r12, 4
+
+	mov rdi, r12
+	call isqrt
+
+	mul rax
+	cmp rax, r12
+	je .true
+	sub r12, 8
+
+	mov rdi, r12
+	call isqrt
+	mul rax
+	cmp rax, r12
+	je .true
+
+	xor rax, rax
+	jmp .done
+.true:
+	mov rax, 1
+.done:
+	pop r12
+	ret
+
+
+isqrt:
+	xor rcx, rcx
+	mov rcx, 1
+	shl rcx, 31 ; max possible sqrt of a 64-bit int is 2^32
+.loop:
+	cmp rcx, rdi
+	jl .break
+	shr rcx, 1
+	jmp .loop
+.break:
+	xor rax, rax
+.calc:
+	or rax, rcx
+	mov r8, rax
+	mov rdx, rax
+	mul rdx
+	cmp rax, rdi
+	mov rax, r8
+	je .done
+	jl .less
+	xor rax, rcx
+.less:
+	shr rcx, 1
+	test rcx, rcx
+	jz .done
+	jmp .calc
 .done:
 	ret
 
--- math_test.c
+++ math_test.c
@@ -3,13 +3,33 @@
 
 extern uint64_t factorial(uint8_t n);
 extern uint64_t fibonacci(uint8_t n);
+extern uint64_t isqrt(uint64_t n);
+extern int is_fib(int n); 
 
 void print_test(uint64_t exp, uint64_t got);
 
 int main()
 {
+	int i, res;
+
 	print_test(2*3*4*5*6*7*8*9*10, factorial(10));
 	print_test(55, fibonacci(10));
+	print_test(7, isqrt(49));
+	print_test(7, isqrt(53));
+	print_test(100, isqrt(10000));
+
+	res = 1;
+	for (i = 0; i < 20; ++i)
+		res = res && 1 == is_fib(fibonacci(i));
+
+	printf("%s\n", res ? "passed! is_fib == 1" : "failed: is_fib == 1");
+
+	res = 0;
+	for (i = 56; i < 89; ++i)
+		res = res || is_fib(i);
+
+	printf("%s\n", res == 0 ? "passed! is_fib == 0" : "failed: is_fib == 0");
+
 	return 0;
 }
 
